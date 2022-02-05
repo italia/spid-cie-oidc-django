c89363ef09371dfaa4cd12ea028882ffadc3b318
peppelinux <giuseppe.demarco@unical.it>
2022-02-06 00:42:36 +0100

    onboarding descendants and contacts

--- spid_cie_oidc/onboarding/admin.py
+++ spid_cie_oidc/onboarding/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FederationDescendant, FederationDescendantContact
+
+
+class FederationDescendantContactAdminInline(admin.TabularInline):
+    model = FederationDescendantContact
+    extra = 0
+    readonly_fields = ('created', 'modified')
+    raw_id_fields = ('entity',)
+
+
+@admin.register(FederationDescendant)
+class FederationDescendantAdmin(admin.ModelAdmin):
+    list_display = ('sub', 'type', 'status', 'is_active', 'created')
+    list_filter = ('type', 'created', 'modified', 'is_active')
+    search_fields = ('sub',)
+    readonly_fields = (
+        "created", "modified"
+    )
+    inlines = (FederationDescendantContactAdminInline, )
--- spid_cie_oidc/onboarding/models.py
+++ spid_cie_oidc/onboarding/models.py
@@ -1,3 +1,142 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from spid_cie_oidc.entity.abstract_models import TimeStampedModel
+
+
+ENTITY_TYPES = (
+    "openid_relying_party",
+    "openid_provider",
+    "federation_entity",
+    "oauth_resource"
+)
+
+ENTITY_STATUS = {
+    "unreachable": False,
+    "valid": True,
+    "signature_failed": False,
+    "not_valid": False,
+    "unknown": None
+}
+
+
+
+class FederationDescendant(TimeStampedModel):
+    """
+        Federation OnBoarding entries.
+    """
+    sub = models.URLField(
+        max_length=255,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text=_(
+            "URL that identifies this Entity in the Federation."
+        ),
+    )
+    type = models.CharField(
+        max_length=33,
+        blank=True,
+        default='openid_relying_party',
+        choices = [(i,i) for i in ENTITY_TYPES],
+        help_text=_("OpenID Connect Federation entity type")
+    )
+    registrant = models.ManyToManyField(
+        get_user_model(),
+        help_text=_(
+            "users that can login and modify sub, contacts and jwks"
+        )
+    )
+    jwks = models.JSONField(
+        blank=False,
+        null=False,
+        help_text=_("a list of public keys"),
+        default=list
+    )
+    trust_marks = models.JSONField(
+        blank=True,
+        help_text=_(
+            "trust marks released for this subject"
+        ),
+        default=list,
+    )
+    metadata_policy = models.JSONField(
+        blank=True,
+        help_text=_("if present overloads the DEFAULT policy"),
+        default=dict,
+    )
+    constraints = models.JSONField(
+        blank=True,
+        help_text=_("if present overloads the DEFAULT policy"),
+        default=dict,
+    )
+    extended_claims = models.JSONField(
+        blank=True,
+        help_text=_(
+            "a dictionary containing any other claim like: "
+            "jti, crti, policy_language_crit and any other extension"
+        ),
+        default=dict,
+    )
+    status = models.CharField(
+        max_length=33,
+        default=False,
+        help_text=_(
+            "Its entity configuration is periodically fetched and validated."
+        ),
+        choices = [(i, i) for i in ENTITY_STATUS.keys()]
+    )
+    status_log = models.JSONField(
+        blank=True,
+        help_text=_("status log"),
+        default=dict,
+    )
+    is_active = models.BooleanField(
+        default=False, help_text=_("If this entity is active. ")
+    )
+
+    class Meta:
+        verbose_name = "Federation Entity Descendant"
+        verbose_name_plural = "Federation Entity Descendant"
+
+    def __str__(self):
+        return "{} [{} and {}]".format(
+            self.sub,
+            self.status,
+            "active" if self.is_active else "--"
+        )
+
+
+class FederationDescendantContact(TimeStampedModel):
+    """
+        Federation OnBoarding entries.
+    """
+    entity = models.ForeignKey(
+        FederationDescendant,
+        on_delete=models.CASCADE,
+        help_text=_("Entity for which this contac is related")
+    )
+    contact = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        help_text=_(
+            "any kind of contact type, usually an email."
+        ),
+    )
+    type = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        choices = [
+            (i, i) for i in ('email', 'telephone', 'other')
+        ]
+    )
+
+    class Meta:
+        verbose_name = "Federation Entity Contact"
+        verbose_name_plural = "Federation Entity Contact"
+
+    def __str__(self):
+        return f"{self.contact} {self.entity.sub}"
