1f11ad631c12181201d482536a2854f75c4fd537
dezhizhang <dezhi@elabor.biz>
2022-02-20 00:26:35 +0100

    chore: create list entities with pagination

--- spid_cie_oidc/onboarding/views.py
+++ spid_cie_oidc/onboarding/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import OnboardingRegistrationForm
+from django.core.paginator import Paginator
 
 
 from spid_cie_oidc.onboarding.models import (
@@ -100,4 +101,12 @@ def onboarding_registration (request):
     return render(request, 'onboarding_registration.html', context)
 
 def onboarding_entities (request):
-    return render(request, 'onboarding_entities.html')
+    entity_list = [
+        {"name_organization": "name organization 1", "url_entity": "https://organization1.it"}, 
+        {"name_organization": "name organization 2", "url_entity": "https://organization2.it"}
+    ]
+    p = Paginator(entity_list, 1)
+    page = request.GET.get('page')
+    entities = p.get_page(page)
+    return render(request, 'onboarding_entities.html', 
+        {'entity_list': entity_list, 'entities': entities })
