249ba4b1025727ef26adb6cd4079c4afc33528d7
dezhizhang <dezhi@elabor.biz>
2022-02-21 12:41:26 +0100

    chore:  add model OnboardingRegistration

---
+++ spid_cie_oidc/onboarding/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import OnBoardingRegistration
+
+
+@admin.register(OnBoardingRegistration)
+class OnBoardingRegistrationAdmin(admin.ModelAdmin):
+    list_display = ("organization_name", "url_entity", "authn_buttons_page_url", "public_jwks")
--- spid_cie_oidc/onboarding/models.py
+++ spid_cie_oidc/onboarding/models.py
@@ -1,1 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext as _
+
+class OnBoardingRegistration (models.Model):
+
+    organization_name = models.CharField(
+        max_length=33,
+        help_text=_("Organization Name. ")
+    )
+
+    url_entity = models.URLField(
+        max_length=255,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text=_("URL of the Entity."),
+    )
+
+    authn_buttons_page_url = models.URLField(
+        max_length=255,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text=_("URL of the page where the SPID/CIE button is available."),
+    )
+
+    public_jwks = models.JSONField(
+        blank=False,
+        help_text=_("Public jwks of the Entities"),
+        default=dict,
+    )
