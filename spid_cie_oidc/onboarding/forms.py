41e7503ec69dc9eff55a2a651aadafa0877bad02
dezhizhang <dezhi@elabor.biz>
2022-02-21 21:22:06 +0100

    fix: handle form registration

--- spid_cie_oidc/onboarding/forms.py
+++ spid_cie_oidc/onboarding/forms.py
@@ -1,22 +1,23 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
+from spid_cie_oidc.entity.validators import validate_public_jwks
 
 class OnboardingRegistrationForm(forms.Form):
     
     organization_name = forms.CharField(
+        initial='',
         label= _("organization Name"),
         error_messages={'required' : _('Enter your organization name')}
     )
 
     url_entity = forms.URLField(
-        initial='https://',
+        initial='',
         label=_("url of the entity"),
         error_messages={'required' : _('Enter your url of the entity')}
     )
 
     authn_buttons_page_url = forms.URLField(
-        initial='http://',
+        initial='',
         label=_("url of the page where the SPID/CIE button is available"),
         error_messages={
             'required' : _('Enter the url of the page where the SPID/CIE button is available')}
@@ -25,5 +26,6 @@ class OnboardingRegistrationForm(forms.Form):
     public_jwks = forms.JSONField(
         label=_("public jwks of the entities"),
         error_messages={
-            'required' : _('Enter the public jwks of the entities')}
+            'required' : _('Enter the public jwks of the entities')},
+        validators = [validate_public_jwks]
     )
--- spid_cie_oidc/onboarding/views.py
+++ spid_cie_oidc/onboarding/views.py
@@ -1,7 +1,12 @@
+import imp
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .forms import OnboardingRegistrationForm
+from .models import OnBoardingRegistration
 
 
 def onboarding_landing (request):
@@ -10,18 +15,22 @@ def onboarding_landing (request):
 
 def onboarding_registration (request):
     form = OnboardingRegistrationForm()
-
-    context ={'form': form}
+    context = {'form': form}
+    if request.method == 'POST':
+        form = OnboardingRegistrationForm(request.POST)
+        if not form.is_valid():
+            context = {'form': form}
+        else:
+            form_dict = {**form.cleaned_data}
+            OnBoardingRegistration.objects.create(**form_dict)
+            messages.success(request, _('Registration successfully'))
+            return redirect('oidc_onboarding_landing')
     return render(request, 'onboarding_registration.html', context)
 
 
 def onboarding_entities (request):
-    entity_list = [
-        {"name_organization": "name organization 1", "url_entity": "https://organization1.it"}, 
-        {"name_organization": "name organization 2", "url_entity": "https://organization2.it"},
-        {"name_organization": "name organization 2", "url_entity": "https://organization2.it"}
-    ]
-    p = Paginator(entity_list, 1)
+    entity_list = OnBoardingRegistration.objects.all()
+    p = Paginator(entity_list, 10)
     page = request.GET.get('page')
     entities = p.get_page(page)
     return render(request, 'onboarding_entities.html', 
