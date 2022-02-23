2ad34c0051c375963692202bf24e9f059e10da86
Francesca <francesca@elabor.biz>
2022-02-23 12:18:42 +0100

    feat: added revocation request validators

---
+++ spid_cie_oidc/onboarding/tests/test_09_revocation_request.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from pydantic import ValidationError
+from spid_cie_oidc.onboarding.tests.revocation_request_settings import (
+    REVOCATION_REQUEST, REVOCATION_REQUEST_NO_CLIENT_ASSERTION,
+    REVOCATION_REQUEST_NO_CLIENT_ASSERTION_TYPE,
+    REVOCATION_REQUEST_NO_CLIENT_ID,
+    REVOCATION_REQUEST_NO_CORRECT_CLIENT_ASSERTION,
+    REVOCATION_REQUEST_NO_CORRECT_CLIENT_ASSERTION_TYPE,
+    REVOCATION_REQUEST_NO_CORRECT_CLIENT_ID,
+    REVOCATION_REQUEST_NO_CORRECT_TOKEN, REVOCATION_REQUEST_NO_TOKEN)
+from spid_cie_oidc.onboarding.validators.revocation_request import RevocationRequest
+
+
+class RevocationRequestTest(TestCase):
+
+    def test_validate_revocation_request(self):
+        RevocationRequest(**REVOCATION_REQUEST)
+
+    def test_validate_revocation_request_no_client_assertion(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CLIENT_ASSERTION)
+
+    def test_validate_revocation_request_no_correct_client_assertion(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CORRECT_CLIENT_ASSERTION)
+
+    def test_validate_revocation_request_no_client_assertion_type(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CLIENT_ASSERTION_TYPE)
+
+    def test_validate_revocation_request_no_correct_client_assertion_type(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CORRECT_CLIENT_ASSERTION_TYPE)
+
+    def test_validate_revocation_request_no_client_id(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CLIENT_ID)
+
+    def test_validate_revocation_request_no_correct_client_id(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CORRECT_CLIENT_ID)
+
+    def test_validate_revocation_request_no_token(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_TOKEN)
+
+    def test_validate_revocation_request_no_correct_token(self):
+        with self.assertRaises(ValidationError):
+            RevocationRequest(**REVOCATION_REQUEST_NO_CORRECT_TOKEN)
