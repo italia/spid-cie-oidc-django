c375f9ed416da0f30bf652094fc41d0c83e4758b
peppelinux <giuseppe.demarco@unical.it>
2022-02-25 16:54:15 +0100

    fix: wrong import in trust chain helpers

---
+++ spid_cie_oidc/entity/trust_chain_operations.py
@@ -0,0 +1,144 @@
+import logging
+
+from . exceptions import InvalidTrustchain
+from . models import FetchedEntityStatement, TrustChain
+from . statements import EntityConfiguration, get_entity_configurations
+from . settings import HTTPC_PARAMS
+from . trust_chain import TrustChainBuilder
+from . utils import datetime_from_timestamp
+
+logger = logging.getLogger(__name__)
+
+
+def trust_chain_builder(
+    subject: str,
+    trust_anchor: EntityConfiguration,
+    httpc_params: dict = HTTPC_PARAMS,
+    required_trust_marks: list = [],
+    metadata_type: str = "openid_provider",
+) -> TrustChainBuilder:
+    """
+    Minimal Provider Discovery endpoint request processing
+
+    metadata_type MUST be one of
+        openid_provider
+        openid_relying_party
+        oauth_resource
+    """
+    tc = TrustChainBuilder(
+        subject,
+        trust_anchor=trust_anchor,
+        required_trust_marks=required_trust_marks,
+        httpc_params=httpc_params,
+        metadata_type=metadata_type
+    )
+    tc.start()
+
+    if not tc.is_valid:
+        logger.error(
+            "The tree of trust cannot be validated for "
+            f"{tc.subject}: {tc.tree_of_trust}"
+        )
+        return False
+    else:
+        return tc
+
+
+def dumps_statements_from_trust_chain_to_db(
+    trust_chain:TrustChainBuilder
+) -> list:
+
+    entity_statements = []
+
+    for stat in trust_chain.trust_path:
+        fes = FetchedEntityStatement.objects.create(
+            sub = stat.sub,
+            iss = stat.iss,
+            exp = datetime_from_timestamp(stat.payload['exp']),
+            iat = datetime_from_timestamp(stat.payload['iat']),
+            statement = stat.payload,
+            jwt = stat.jwt
+        )
+        entity_statements.append(fes)
+        if stat.verified_descendant_statements:
+
+            for desc_stat_sub in stat.verified_descendant_statements:
+                payload = stat.verified_descendant_statements[desc_stat_sub]
+                jwt = stat.verified_descendant_statements_as_jwt[desc_stat_sub]
+
+                desc_fes = FetchedEntityStatement.objects.create(
+                    sub = payload['sub'],
+                    iss = payload['iss'],
+                    exp = datetime_from_timestamp(payload['exp']),
+                    iat = datetime_from_timestamp(payload['iat']),
+                    statement = payload,
+                    jwt = jwt
+                )
+                entity_statements.append(desc_fes)
+
+    return entity_statements
+
+
+def get_or_create_trust_chain(
+    subject:str,
+    trust_anchor:str,
+    httpc_params: dict = HTTPC_PARAMS,
+    required_trust_marks: list = [],
+    metadata_type:str = "openid_provider"
+) -> TrustChain:
+
+    fetched_trust_anchor = FetchedEntityStatement.objects.filter(
+        sub = trust_anchor, iss = trust_anchor
+    ).first()
+
+    if not fetched_trust_anchor or fetched_trust_anchor.is_expired():
+        jwts = get_entity_configurations(
+            [trust_anchor], httpc_params = httpc_params
+        )
+        ta_conf = EntityConfiguration(jwts[0], httpc_params=httpc_params)
+        # trust to the anchor should be absolute!
+        # ta_conf.validate_by_itself()
+        res = FetchedEntityStatement.objects.create(
+            sub = ta_conf.sub,
+            iss = ta_conf.iss,
+            exp = datetime_from_timestamp(ta_conf.payload['exp']),
+            iat = datetime_from_timestamp(ta_conf.payload['iat']),
+            statement = ta_conf.payload,
+            jwt = ta_conf.jwt
+        )
+        fetched_trust_anchor = res
+    else:
+        ta_conf = fetched_trust_anchor.get_entity_configuration_as_obj()
+
+    tc = TrustChain.objects.filter(
+        sub = subject,
+        trust_anchor = fetched_trust_anchor
+    ).first()
+
+    if not tc or not tc.is_active or tc.is_expired:
+        trust_chain = trust_chain_builder(
+            subject=subject,
+            trust_anchor=ta_conf,
+            required_trust_marks = required_trust_marks,
+            metadata_type=metadata_type
+        )
+        if not trust_chain.is_valid:
+            raise InvalidTrustchain(
+                f"Trust chain for subject {subject} and "
+                f"trust_anchor {trust_anchor} is not valid"
+            )
+        res = dumps_statements_from_trust_chain_to_db(trust_chain)
+
+        tc = TrustChain.objects.create(
+            sub = subject,
+            type = metadata_type,
+            exp = trust_chain.exp_datetime,
+            chain = trust_chain.serialize(),
+            metadata = trust_chain.final_metadata,
+            parties_involved = [i.sub for i in trust_chain.trust_path],
+            status = 'valid',
+            trust_anchor = fetched_trust_anchor,
+            is_active = True
+        )
+
+    return tc
