00467e0752394534d5c2da1b2255d326ebedeb07
peppelinux <giuseppe.demarco@unical.it>
2022-02-25 19:19:55 +0100

    fix: rp tests

--- spid_cie_oidc/relying_party/tests/test_02_rp_commands.py
+++ spid_cie_oidc/relying_party/tests/test_02_rp_commands.py
@@ -12,8 +12,8 @@ class OidcRPCliTests(TestCase):
     
     def test_fetch_openid_providers(self):
         overrides = dict(
-            OIDCFED_IDENTITY_PROVIDERS = ,
-            OIDCFED_REQUIRED_TRUST_MARKS = 
+            OIDCFED_IDENTITY_PROVIDERS = "",
+            OIDCFED_REQUIRED_TRUST_MARKS = ""
         )
         
         with self.settings(**overrides):
