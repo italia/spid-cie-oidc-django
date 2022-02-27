86724c875b0b3348918739ff64d89e22f38180b6
Francesca <francesca@elabor.biz>
2022-02-27 10:08:13 +0100

    chore: code linting

--- spid_cie_oidc/provider/settings.py
+++ spid_cie_oidc/provider/settings.py
@@ -11,33 +11,33 @@ JWT_CLIENT_ASSERTION = {
 }
 
 PAYLOAD = {
-    'client_id': 'http://127.0.0.1:8000/oidc/rp/', 
+    'client_id': 'http://127.0.0.1:8000/oidc/rp/',
     'sub': 'http://rp-test/oidc/rp',
     'iss': 'http://rp-test/oidc/rp',
-    'response_type': 'code', 
-    'scope': 'openid', 
-    'code_challenge': 'qWJlMe0xdbXrKxTm72EpH659bUxAxw80', 
-    'code_challenge_method': 'S256', 
-    'nonce': 'MBzGqyf9QytD28eupyWhSqMj78WNqpc2', 
-    'prompt': 'consent login', 
-    'redirect_uri': 'https://rp.cie.it/callback1/', 
-    'acr_values': 'CIE_L1 CIE_L2', 
+    'response_type': 'code',
+    'scope': 'openid',
+    'code_challenge': 'qWJlMe0xdbXrKxTm72EpH659bUxAxw80',
+    'code_challenge_method': 'S256',
+    'nonce': 'MBzGqyf9QytD28eupyWhSqMj78WNqpc2',
+    'prompt': 'consent login',
+    'redirect_uri': 'https://rp.cie.it/callback1/',
+    'acr_values': 'CIE_L1 CIE_L2',
     'claims': {
         'id_token': {
-            'family_name': {'essential': True}, 
+            'family_name': {'essential': True},
             'email': {'essential': True}
-        }, 
+        },
         'userinfo': {
-            'name': None, 
+            'name': None,
             'family_name': None
         }
-    }, 
+    },
     'state': 'fyZiOL9Lf2CeKuNT2JzxiLRDink0uPcd',
     "iat": iat_now(),
     "exp": exp_from_now(33),
     "aud": ["https://rp.spid.agid.gov.it/auth"],
     "jti": "a72d5df0-2415-4c7c-a44f-3988b354040b",
-    }
+}
 
 TA_STATMENT = {
     "sub": "http://127.0.0.1:8000/",
