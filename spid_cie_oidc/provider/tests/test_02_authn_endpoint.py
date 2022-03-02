88ea51cd4ecd868d4027e5f5b55fee2b134eea2b
Francesca <francesca@elabor.biz>
2022-03-02 15:39:04 +0100

    fix: delete authn_endpoint_settings.py

--- spid_cie_oidc/provider/tests/test_02_authn_endpoint.py
+++ spid_cie_oidc/provider/tests/test_02_authn_endpoint.py
@@ -14,13 +14,47 @@ from spid_cie_oidc.entity.utils import (
     iat_now
 )
 
-from .authn_endpoint_settings import REQUEST_OBJECT_PAYLOAD
+# TODO: we need factory function to get fresh now
+IAT = iat_now()
+EXP = exp_from_now()
+
+REQUEST_OBJECT_PAYLOAD = {
+    'client_id': 'http://127.0.0.1:8000/oidc/rp/',
+    'sub': 'http://rp-test/oidc/rp',
+    'iss': 'http://rp-test/oidc/rp',
+    'response_type': 'code',
+    'scope': ['openid'],
+    'code_challenge': 'qWJlMe0xdbXrKxTm72EpH659bUxAxw80',
+    'code_challenge_method': 'S256',
+    'nonce': 'MBzGqyf9QytD28eupyWhSqMj78WNqpc2',
+    'prompt': 'consent login',
+    'redirect_uri': 'http://rp-test/oidc/rp/callback1/',
+    'acr_values': ['https://www.spid.gov.it/SpidL1', 'https://www.spid.gov.it/SpidL2'],
+    'claims': {
+        'id_token': {
+            'family_name': {'essential': True},
+            'email': {'essential': True}
+        },
+        'userinfo': {
+            'given_name': None,
+            'family_name': None
+        }
+    },
+    'state': 'fyZiOL9Lf2CeKuNT2JzxiLRDink0uPcd',
+    'iss': "https://op.spid.agid.gov.it/",
+    'aud': ["https://rp.spid.agid.gov.it/auth"],
+    'iat': IAT,
+    'exp': EXP,
+    'jti': "a72d5df0-2415-4c7c-a44f-3988b354040b"
+}
+
 
 
 class AuthnRequestTest(TestCase):
 
     def setUp(self):
         self.req = HttpRequest()
+        breakpoint()
         self.rp_jwk = RP_METADATA["openid_relying_party"]['jwks']['keys'][0]
         User.objects.create(first_name ="test", last_name= "test", email="test@test.it")
 
@@ -53,5 +87,7 @@ class AuthnRequestTest(TestCase):
         self.assertTrue(res.status_code == 200)
         self.assertIn("username", res.content.decode())
         self.assertIn("password", res.content.decode())
+        res = client.post(res.url, {"username": "test", "password":"test"})
+        self.assertTrue(res.status_code == 302)
 
 
