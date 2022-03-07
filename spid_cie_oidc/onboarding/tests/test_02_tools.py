b95c19d10ad1d8d3276f04d51130cf46aa890a7b
dezhizhang <dezhi@elabor.biz>
2022-03-07 15:04:56 +0100

    feat: tests for tools onboarding

--- spid_cie_oidc/onboarding/tests/test_02_tools.py
+++ spid_cie_oidc/onboarding/tests/test_02_tools.py
@@ -4,45 +4,66 @@ import json
 
 from spid_cie_oidc.entity.models import *
 from spid_cie_oidc.onboarding.urls import *
-from spid_cie_oidc.entity.jwks import serialize_rsa_key,new_rsa_key
-from spid_cie_oidc.onboarding.tests.tools_settings import jwk_priv, jwk_pub, jwt
-from spid_cie_oidc.onboarding.forms import OnboardingCreateTrustChain
+from spid_cie_oidc.onboarding.tests.tools_settings import jwk_priv, jwt
+from spid_cie_oidc.entity.tests.settings import ta_conf_data
+from spid_cie_oidc.authority.models import (
+    FederationEntityConfiguration,
+    FederationEntityProfile,
+    FederationDescendant,
+    FederationDescendantJwk,
+    FederationEntityAssignedProfile
+)
+from spid_cie_oidc.authority.tests.settings import (
+    rp_conf,
+    RP_PROFILE,
+    rp_onboarding_data
+)
 
-
-from unittest.mock import patch
+from spid_cie_oidc.authority.tests.settings import RP_METADATA_JWK1, RP_METADATA_JWK1_pub
 
 class ToolsTests(TestCase):
 
     def setUp(self):
 
         self.client = Client()
-        self.data = {"organization_name" : "","url_entity" : "",
-            "authn_buttons_page_url" : "","public_jwks" : ""
-        }
+        self.ta_conf = FederationEntityConfiguration.objects.create(**ta_conf_data)
+        self.rp_conf = FederationEntityConfiguration.objects.create(**rp_conf)
 
+        self.rp_profile = FederationEntityProfile.objects.create(**RP_PROFILE)
+        self.rp = FederationDescendant.objects.create(**rp_onboarding_data)
+
+        self.rp_jwk = PublicJwk.objects.create(
+            jwk=self.rp_conf.public_jwks[0], kid=self.rp_conf.public_jwks[0]["kid"]
+        )
+        FederationDescendantJwk.objects.create(descendant=self.rp, jwk=self.rp_jwk)
+        self.rp_assigned_profile = FederationEntityAssignedProfile.objects.create(
+            descendant=self.rp, profile=self.rp_profile, issuer=self.ta_conf
+        )
     def test_create_jwk(self):
         url = reverse("oidc_onboarding_create_jwk")
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
         self.assertIsNotNone(res.context['jwk'])
 
-    # def test_convert_jwk_to_pem(self):
-    #     url = reverse("oidc_onboarding_convert_jwk")
-    #     res = self.client.get(url)
-    #     self.assertEqual(res.status_code, 200)
-    #     #check with jwk error
-    #     res = self.client.post(url + '?type=private', {'kty': 'RSA'})
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertIn("alert-error", res.content.decode())
-    #     #check with public jwk the function jwk priv to pem
-    #     res = self.client.post(url + '?type=private', jwk_pub)
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertIn("alert-error", res.content.decode())
-    #     #check with right jwk
-    #     res = self.client.post(url + '?type=private', jwk_priv)
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertNotIn("alert-error", res.content.decode())
-    #     #breakpoint()
+    def test_convert_jwk_to_pem(self):
+        url = reverse("oidc_onboarding_convert_jwk")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+        #check with jwk error
+        res = self.client.post(url + '?type=private', {'kty': 'RSA'})
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("alert-error", res.content.decode())
+
+        #check with public jwk the function jwk priv to pem
+        res = self.client.post(url + '?type=private', {'jwk':json.dumps(RP_METADATA_JWK1_pub)})
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("alert-error", res.content.decode())
+        
+        #check with right jwk
+        res = self.client.post(url + '?type=private', {'jwk':json.dumps(RP_METADATA_JWK1)})
+        self.assertEqual(res.status_code, 200)
+        self.assertNotIn("alert-error", res.content.decode())
 
     def test_decode_and_verify_jwt(self):
         url = reverse("oidc_onboarding_tools_decode_jwt")
@@ -91,24 +112,26 @@ class ToolsTests(TestCase):
         url = reverse("oidc_onboarding_tools_validating_trustmark")
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
-        # res = self.client.get(url, {
-        #     "sub":"http://127.0.0.1:8000/oidc/",
-        #     "type": "openid_provider",
-        #     "anchor": "http://127.0.0.1:8000/"
-        # })
-        # self.assertEqual(res.status_code, 200)
-        # self.assertIn("alert-error", res.content.decode())
-        # self.assertIn(
-        #     "Failed to resolve entity statement, Please check your inserted data", 
-        #     res.content.decode())
 
-        # form_data= {
-        #     "sub": "http://127.0.0.1:8000/oidc/op/",
-        #     "type": "openid_provider",
-        #     "anchor": "http://127.0.0.1:8000/",
-        # }
-        # res = self.client.get(url, form_data)
-        # self.assertEqual(res.status_code, 200)
-        # self.assertIsNotNone(res.context['resolved_statement'])
+        res = self.client.get(url, {
+            "id": "https://www.ciao.gov.it/certification/rp",
+            "sub": "http://ciao.it/oidc/rp/",
+        })
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("alert-error", res.content.decode())
 
-    
+        res = self.client.get(url, {
+            "id": "https://www.spid.gov.it/certification/rp",
+            "sub": "http://rp-test.it/oidc/rp/",
+        })
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("alert-success", res.content.decode())
+
+        trust_mark = self.rp_assigned_profile.trust_mark_as_jws
+        
+        res = self.client.get(url, {
+            "trust_mark": trust_mark,
+        })
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("alert-success", res.content.decode())
+
