330f6e29088781c07deb3809a698d6b026b64a32
peppelinux <giuseppe.demarco@unical.it>
2022-03-10 23:10:42 +0100

    fix: moved ORM I/O from tests globals()

--- spid_cie_oidc/relying_party/tests/test_06_fetch_openid_providers.py
+++ spid_cie_oidc/relying_party/tests/test_06_fetch_openid_providers.py
@@ -16,21 +16,26 @@ from spid_cie_oidc.provider.tests.settings import op_conf
 
 EXP = datetime_from_timestamp(exp_from_now(33))
 NOW = datetime_from_timestamp(iat_now())
-ta_fes = FetchedEntityStatement.objects.create(
-    sub=TA_SUB,
-    iss=TA_SUB,
-    exp=EXP,
-    iat=NOW,
-)
-TC = TrustChain.objects.create(
-    sub=op_conf["sub"],
-    type="openid_provider",
-    exp=EXP,
-    metadata=op_conf["metadata"]["openid_provider"],
-    status="valid",
-    trust_anchor=ta_fes,
-    is_active=True,
-)
+
+
+def create_tc():
+    ta_fes = FetchedEntityStatement.objects.create(
+        sub=TA_SUB,
+        iss=TA_SUB,
+        exp=EXP,
+        iat=NOW
+    )
+    return TrustChain.objects.create(
+        sub=op_conf["sub"],
+        type="openid_provider",
+        exp=EXP,
+        metadata=op_conf["metadata"]["openid_provider"],
+        status="valid",
+        trust_anchor=ta_fes,
+        is_active=True,
+    )
+
+
 class FetchProviderTest(TestCase):
 
     def setUp(self):
@@ -49,7 +54,7 @@ class FetchProviderTest(TestCase):
             **kwargs,
         )
     
-    @patch("spid_cie_oidc.entity.trust_chain_operations.get_or_create_trust_chain", return_value = TC)
+    @patch("spid_cie_oidc.entity.trust_chain_operations.get_or_create_trust_chain", return_value = create_tc())
     @override_settings(OIDCFED_IDENTITY_PROVIDERS = {"http://127.0.0.1:8000/oidc/op/" :"http://testserver/"})
     def test_fetch_provider(self, mocked):
         out = self.exec('fetch_openid_providers', '--start')
