59df715a3b66529441db4201f343bd8db112f627
Francesca <francesca@elabor.biz>
2022-03-11 13:45:51 +0100

    test: rep landing

---
+++ spid_cie_oidc/relying_party/tests/test_07_oidc_rp_landing.py
@@ -0,0 +1,42 @@
+
+
+from django.test import Client, TestCase
+from django.urls import reverse
+from spid_cie_oidc.entity.models import FetchedEntityStatement, TrustChain
+from spid_cie_oidc.entity.tests.settings import TA_SUB
+from spid_cie_oidc.entity.utils import (
+    datetime_from_timestamp, 
+    exp_from_now,
+    iat_now
+)
+from spid_cie_oidc.provider.tests.settings import op_conf
+
+
+class RpLandingTest(TestCase):
+
+    def setUp(self) -> None:
+        NOW = datetime_from_timestamp(iat_now())
+        EXP = datetime_from_timestamp(exp_from_now(33))
+        self.ta_fes = FetchedEntityStatement.objects.create(
+            sub=TA_SUB,
+            iss=TA_SUB,
+            exp=EXP,
+            iat=NOW,
+        )
+
+        self.trust_chain = TrustChain.objects.create(
+            sub=op_conf["sub"],
+            type="openid_provider",
+            exp=EXP,
+            metadata=op_conf["metadata"]["openid_provider"],
+            status="valid",
+            trust_anchor=self.ta_fes,
+            is_active=True,
+        )
+
+    def test_rp_landing(self):
+        client = Client()
+        url = reverse("spid_cie_rp_landing")
+        res = client.get(url)
+        self.assertTrue(res.status_code == 200)
+        
