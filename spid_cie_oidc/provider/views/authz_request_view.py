08333ad928c207a667e455cb57393e2ad83e7613
dezhizhang <dezhi@elabor.biz>
2022-03-25 14:32:59 +0100

    chore: view request params

--- spid_cie_oidc/provider/views/authz_request_view.py
+++ spid_cie_oidc/provider/views/authz_request_view.py
@@ -2,6 +2,7 @@ import hashlib
 import logging
 import urllib.parse
 import uuid
+import json
 
 from django.conf import settings
 from django.contrib.auth import authenticate, login
@@ -137,13 +138,15 @@ class AuthzRequestView(OpBase, View):
 
         # stores the authz request in a hidden field in the form
         form = self.get_login_form()()
+        #breakpoint()
         context = {
             "client_organization_name": tc.metadata.get(
                 "client_name", self.payload["client_id"]
             ),
             "hidden_form": AuthzHiddenForm(dict(authz_request_object=req)),
             "form": form,
-            "redirect_uri": self.payload["redirect_uri"]
+            "redirect_uri": self.payload["redirect_uri"],
+            "obj_request": json.dumps(self.payload, indent=2)
         }
         return render(request, self.template, context)
 
