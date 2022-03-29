3cb952f4681d15ea7eac23e8bd5bf901ab7b0fd0
dezhizhang1985 <84180831+dezhizhang1985@users.noreply.github.com>
2022-03-29 22:07:32 +0200

    chore: buttons provider (#192)
    * chore: buttons provider
    
    * fix : defautlt provider button for empty TC

--- spid_cie_oidc/relying_party/views/rp_landing.py
+++ spid_cie_oidc/relying_party/views/rp_landing.py
@@ -1,6 +1,6 @@
 import logging
 import random
-
+from copy import deepcopy
 from django.conf import settings
 from django.shortcuts import render
 from spid_cie_oidc.entity.models import TrustChain
@@ -9,21 +9,45 @@ logger = logging.getLogger(__name__)
 
 
 def oidc_rp_landing(request):
-    trust_chains = TrustChain.objects.filter(
+    spid_providers = {
+        k: {"sub": v} for k, v in 
+        settings.OIDCFED_IDENTITY_PROVIDERS.get("spid", {}).items()
+    }
+    cie_providers = {
+        k: {"sub": v} for k, v in 
+        settings.OIDCFED_IDENTITY_PROVIDERS.get("cie", {}).items()
+    }
+    
+    tc_spid = TrustChain.objects.filter(
+        sub__in = spid_providers.keys(),
         metadata__openid_provider__isnull=False,
         is_active=True
     )
-    spid_providers = []
-    cie_providers = []
-    for tc in trust_chains:
-        if tc.is_active:
-            if tc.sub in settings.OIDCFED_IDENTITY_PROVIDERS.get("spid", []):
-                spid_providers.append(tc)
-            elif tc.sub in settings.OIDCFED_IDENTITY_PROVIDERS.get("cie", []):
-                cie_providers.append(tc)
-    random.shuffle(spid_providers)
+
+    for i in tc_spid:
+        spid_providers[i.sub] = {
+            "sub": i.sub,
+            "logo_uri" : i.metadata.get("openid_provider", {}).get("logo_uri", ""),
+            "organization_name": i.metadata.get("openid_provider", {}).get("organization_name","")
+        }
+
+    tc_cie = TrustChain.objects.filter(
+        sub__in = cie_providers.keys(),
+        metadata__openid_provider__isnull=False,
+        is_active=True
+    )
+
+    for i in tc_cie:
+        cie_providers[i.sub] = {
+            "sub": i.sub,
+            "logo_uri" : i.metadata.get("openid_provider", {}).get("logo_uri", ""),
+            "organization_name": i.metadata.get("openid_provider", {}).get("organization_name","")
+        }
+
+    s_spid_providers = list(spid_providers.items())
+    random.shuffle(s_spid_providers)
     content = {
-        "spid_providers": spid_providers,
+        "spid_providers": dict(s_spid_providers),
         "cie_providers" : cie_providers
     }
     return render(request, "rp_landing.html", content)
