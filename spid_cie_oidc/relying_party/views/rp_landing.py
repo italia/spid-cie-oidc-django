8e33cbecd6fed80aadaa668e7fac5fafe4ba4aaf
dezhizhang <dezhi@elabor.biz>
2022-03-29 11:53:38 +0200

    chore: buttons provider

--- spid_cie_oidc/relying_party/views/rp_landing.py
+++ spid_cie_oidc/relying_party/views/rp_landing.py
@@ -9,18 +9,27 @@ logger = logging.getLogger(__name__)
 
 
 def oidc_rp_landing(request):
-    trust_chains = TrustChain.objects.filter(
-        metadata__openid_provider__isnull=False,
-        is_active=True
-    )
     spid_providers = []
     cie_providers = []
-    for tc in trust_chains:
-        if tc.is_active:
-            if tc.sub in settings.OIDCFED_IDENTITY_PROVIDERS.get("spid", []):
-                spid_providers.append(tc)
-            elif tc.sub in settings.OIDCFED_IDENTITY_PROVIDERS.get("cie", []):
-                cie_providers.append(tc)
+
+    for s in settings.OIDCFED_IDENTITY_PROVIDERS.get("spid", []):
+        t = TrustChain.objects.filter(
+            sub = s,
+            metadata__openid_provider__isnull=False,
+            is_active=True
+        )
+        provider = dict(trust = t.first())
+        spid_providers.append(provider)
+            
+    for c in settings.OIDCFED_IDENTITY_PROVIDERS.get("cie", []):
+        t = TrustChain.objects.filter(
+            sub = c,
+            metadata__openid_provider__isnull=False,
+            is_active=True
+        )
+        provider = dict(trust = t.first())
+        cie_providers.append(provider)
+
     random.shuffle(spid_providers)
     content = {
         "spid_providers": spid_providers,
