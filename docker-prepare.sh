e47e37db68c28e1a9b2575cff5f1ceba1b12416b
peppelinux <giuseppe.demarco@unical.it>
2022-03-31 22:33:56 +0200

    README

--- docker-prepare.sh
+++ docker-prepare.sh
@@ -4,11 +4,15 @@
 export EXPFOLDER="examples-docker"
 cp -R examples $EXPFOLDER
 
+# remove dev db
+rm $EXPFOLDER/relying_party/db.sqlite3 
+rm $EXPFOLDER/provider/db.sqlite3 
+rm $EXPFOLDER/federation_authority/db.sqlite3 
+
 # Configure the rewrite rules:
 export SUB_AT='s\http://127.0.0.1:8000/\http://trust-anchor.org:8000/\g'
 export SUB_RP='s\http://127.0.0.1:8001/\http://relying-party.org:8001/\g'
 export SUB_OP='s\http://127.0.0.1:8002/\http://cie-provider.org:8002/\g'
-
 
 # Apply the rewrite rules:
 
