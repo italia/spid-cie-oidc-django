bdb7c715d9a949e8673e89105edb3874461171b2
peppelinux <giuseppe.demarco@unical.it>
2022-04-04 20:31:38 +0200

    feat: automatic update trust marks via entity admin action

--- spid_cie_oidc/entity/admin.py
+++ spid_cie_oidc/entity/admin.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+import logging
+
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.contrib import messages
@@ -5,12 +8,18 @@ from django.contrib import messages
 # from prettyjson import PrettyJSONWidget
 
 from django.conf import settings
+from .jwtse import unpad_jwt_payload
+from .statements import EntityConfiguration, get_entity_configurations, get_entity_statements
 from .models import (
     FederationEntityConfiguration,
     FetchedEntityStatement,
     TrustChain
 )
 from spid_cie_oidc.entity.trust_chain_operations import get_or_create_trust_chain
+
+from . settings import HTTPC_PARAMS
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(FederationEntityConfiguration)
@@ -22,6 +31,76 @@ class FederationEntityConfigurationAdmin(admin.ModelAdmin):
     # )
     # }
     # }
+
+    @admin.action(description='update trust marks')
+    def update_trust_marks(modeladmin, request, queryset):
+        """
+        fetch trust marks from all the authorities
+        """
+        trust_marks = {}
+
+        for obj in queryset:
+            jwts = get_entity_configurations(obj.authority_hints, HTTPC_PARAMS)
+            for jwt in jwts:
+                try:
+                    ec = EntityConfiguration(jwt, httpc_params=HTTPC_PARAMS)
+                except Exception as e:
+                    _msg = f"Failed getting Entity Configuration for {jwt}: {e}"
+                    logger.warning(_msg)
+                    messages.error(_msg)
+                    continue
+
+                try:
+                    # get superior fetch url
+                    fetch_api_url = ec.payload["metadata"]["federation_entity"][
+                        "federation_fetch_endpoint"
+                    ]
+                except KeyError:
+                    _msg = (
+                        "Missing federation_fetch_endpoint in  "
+                        f"federation_entity metadata for {obj.sub} by {ec.sub}."
+                    )
+                    logger.warning(_msg)
+                    messages.error(_msg)
+                    continue
+                
+                _url = f"{fetch_api_url}?sub={obj.sub}"
+                try:
+                    logger.info(f"Getting entity statements from {_url}")
+                    _jwts = get_entity_statements([_url], HTTPC_PARAMS)
+                    payload = unpad_jwt_payload(_jwts[0])
+                    for i in payload.get("trust_marks", []):
+                        trust_marks[i['id']] = i['trust_mark']
+                except Exception as e:
+                    _msg = f"Error getting entity statements from {_url}: {e}"
+                    logger.warning(_msg)
+                    messages.error(_msg)
+                    continue
+
+            positions = {}
+            count = 0
+            for i in obj.trust_marks:
+                positions[i['id']] = count
+                count += 1
+            
+            if positions:
+                for k,v in trust_marks.items():
+                    if positions.get(k, None):
+                        obj.trust_marks[positions[k]] = {k:v}
+                    else:
+                        obj.trust_marks.append({k:v})
+            else:
+                obj.trust_marks = [
+                    {"id":k, "trust_mark":v} for k,v in trust_marks.items()
+                ]
+            
+            obj.save()
+            messages.success(
+                request, 
+                f"Trust mark reloaded succesfully: {', '.join(trust_marks.keys())}"
+            )
+
+
     list_display = (
         "sub",
         "type",
@@ -39,7 +118,8 @@ class FederationEntityConfigurationAdmin(admin.ModelAdmin):
         "kids",
         "type",
     )
-
+    actions = [update_trust_marks]
+    
     def pems_as_html(self, obj):
         res = ""
         data = dict()
