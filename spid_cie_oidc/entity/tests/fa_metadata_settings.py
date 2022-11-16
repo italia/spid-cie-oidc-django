3392cc43ce38478fc93768ae04b1502cd21f89c1
Giuseppe De Marco <demarcog83@gmail.com>
2022-11-16 15:53:45 +0100

    fix: [DRAFT 25] federation_status_endpoint -> federation_trust_mark_status_endpoint

--- spid_cie_oidc/entity/tests/fa_metadata_settings.py
+++ spid_cie_oidc/entity/tests/fa_metadata_settings.py
@@ -2,7 +2,7 @@ FA_METADATA = {
     "contacts": ["spid.tech@agid.gov.it"],
     "federation_fetch_endpoint": "https://registry.spid.agid.gov.it/oidc/fedapi/",
     "federation_list_endpoint": "https://registry.spid.agid.gov.it/oidc/list/",
-    "federation_status_endpoint": "https://registry.spid.agid.gov.it/oidc/status/",
+    "federation_trust_mark_status_endpoint": "https://registry.spid.agid.gov.it/oidc/status/",
     "federation_resolve_endpoint": "https://registry.spid.agid.gov.it/oidc/resolve/",
     "homepage_uri": "https://spid.agid.gov.it/",
     "organization_name": "Tal dei tali"
