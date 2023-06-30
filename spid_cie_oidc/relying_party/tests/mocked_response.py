dedbb8ba43cb266981a0b0ab3d242d6595abad5f
rglauco <37829079+rglauco@users.noreply.github.com>
2023-06-30 07:52:24 +0200

    test: RP at_hash verification

--- spid_cie_oidc/relying_party/tests/mocked_response.py
+++ spid_cie_oidc/relying_party/tests/mocked_response.py
@@ -63,7 +63,7 @@ class MockedTokenEndPointNoCorrectResponse:
         id_token = {
             'sub': '2ed008b45e66ce53e48273dca5a4463bc8ebd036ebaa824f4582627683c2451b', 
             'nonce': 'ljbvL3rpscgS4ZGda7cgibXHr7vrNREW', 
-            'at_hash': 'u2RjeYbZSJZO55XwY2LJew', 
+            'at_hash': '',
             'c_hash': 'tij0h-zL_bSrsVXy-d3qHw', 
             'aud': [rp_conf["metadata"]["openid_relying_party"]["client_id"],], 
             'iss': op_conf["sub"], 
@@ -82,6 +82,7 @@ class MockedTokenEndPointNoCorrectResponse:
             'iat': iat_now()
         }
         jwt_at = create_jws(access_token, op_conf_priv_jwk, typ="at+jwt")
+        id_token['at_hash'] = left_hash(jwt_at, "HS256")
         jwt_id = create_jws(id_token, op_conf_priv_jwk)
 
         self.access_token = jwt_at
@@ -106,7 +107,7 @@ class MockedTokenEndPointNoCorrectIdTokenResponse:
         id_token = {
             'sub': '2ed008b45e66ce53e48273dca5a4463bc8ebd036ebaa824f4582627683c2451b', 
             'nonce': 'ljbvL3rpscgS4ZGda7cgibXHr7vrNREW', 
-            'at_hash': 'u2RjeYbZSJZO55XwY2LJew', 
+            'at_hash': '',
             'c_hash': 'tij0h-zL_bSrsVXy-d3qHw', 
             'aud': [rp_conf["metadata"]["openid_relying_party"]["client_id"],], 
             'iss': op_conf["sub"], 
@@ -125,6 +126,7 @@ class MockedTokenEndPointNoCorrectIdTokenResponse:
             'iat': iat_now()
         }
         jwt_at = create_jws(access_token, op_conf_priv_jwk, typ="at+jwt")
+        id_token['at_hash'] = left_hash(jwt_at, "HS256")
         jwt_id = create_jws(id_token, INTERMEDIARY_JWK1)
 
         self.access_token = jwt_at
@@ -138,6 +140,50 @@ class MockedTokenEndPointNoCorrectIdTokenResponse:
                 "scope": "openid",
             }
         return json.dumps(res).encode()
+
+
+class MockedTokenEndPointNoCorrectAtHashResponse:
+    def __init__(self):
+        self.status_code = 200
+
+    @property
+    def content(self):
+        id_token = {
+            'sub': '2ed008b45e66ce53e48273dca5a4463bc8ebd036ebaa824f4582627683c2451b',
+            'nonce': 'ljbvL3rpscgS4ZGda7cgibXHr7vrNREW',
+            'at_hash': '45e66ce53e48273dca5a446',
+            'c_hash': 'tij0h-zL_bSrsVXy-d3qHw',
+            'aud': [rp_conf["metadata"]["openid_relying_party"]["client_id"],],
+            'iss': op_conf["sub"],
+            'jti': '402e61bd-950c-4934-83d4-c09a05468828',
+            'exp': exp_from_now(),
+            'iat': iat_now()
+        }
+        access_token = {
+            'iss': op_conf["sub"],
+            'sub': '2ed008b45e66ce53e48273dca5a4463bc8ebd036ebaa824f4582627683c2451b',
+            'aud': [rp_conf["metadata"]["openid_relying_party"]["client_id"],],
+            'client_id': rp_conf["metadata"]["openid_relying_party"]["client_id"],
+            'scope': 'openid',
+            'jti': '402e61bd-950c-4934-83d4-c09a05468828',
+            'exp': exp_from_now(),
+            'iat': iat_now()
+        }
+        jwt_at = create_jws(access_token, op_conf_priv_jwk, typ="at+jwt")
+        jwt_id = create_jws(id_token, op_conf_priv_jwk)
+
+        self.access_token = jwt_at
+        self.id_token = jwt_id
+
+        res = {
+                "access_token": jwt_at,
+                "id_token": jwt_id,
+                "token_type": "Bearer",
+                "expires_in": 3600,
+                "scope": "openid",
+            }
+        return json.dumps(res).encode()
+
 
 class MockedUserInfoResponse:
     def __init__(self):
