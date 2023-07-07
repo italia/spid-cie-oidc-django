c96f1d799684baaf7afd34d511242490887fcd3c
peppelinux <demarcog83@gmail.com>
2023-07-07 16:16:06 +0200

    fix: bump dep versions

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ SRC_FOLDER = '.'
 PKG_NAME = 'spid_cie_oidc'
 
 INSTALL_REQUIRES = [
-    "Django>=4.0,<=5.0",
-    "cryptojwt>=1.8.2,<=2.0",
-    "pydantic>=1.8.2,<=2.0",
+    "Django>=4.0,<5.0",
+    "cryptojwt>=1.8.2,<2.0",
+    "pydantic>=1.8.2,<2.0",
     "pytz>=2021.3",
-    "aiohttp",
-    "requests>=2.20,<=3.0",
+    "aiohttp>=3.8,<3.9",
+    "requests>=2.20,<3.0",
     "pydantic[email]",
     "djagger"
 ]
