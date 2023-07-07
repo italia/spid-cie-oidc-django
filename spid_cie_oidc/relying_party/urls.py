fe3f765d00e3a25dba8bf0d5598b78ded56047e1
rglauco <37829079+rglauco@users.noreply.github.com>
2023-07-07 12:15:07 +0200

    feat: added RP token calls common method and introspection view

--- spid_cie_oidc/relying_party/urls.py
+++ spid_cie_oidc/relying_party/urls.py
@@ -8,7 +8,8 @@ from .views.rp_begin import SpidCieOidcRpBeginView
 from .views.rp_callback import SpidCieOidcRpCallbackView
 from .views.rp_extend_session import SpidCieOidcRefreshToken
 from .views.rp_callback_echo_attributes import SpidCieOidcRpCallbackEchoAttributes
-from .views.rp_initiated_logout import oidc_rpinitiated_logout
+from .views.rp_initiated_logout import SpidCieOidcRpLogout #oidc_rpinitiated_logout
+from .views.rp_introspection import SpidCieOidcRpIntrospection
 from .views.rp_landing import oidc_rp_landing
 from django.contrib.auth.decorators import login_required
 _PREF = "oidc/rp"
@@ -44,9 +45,26 @@ urlpatterns += (
     ),
 )
 
+
+urlpatterns += (
+    path(
+        f"{_PREF}/introspection",
+        login_required(SpidCieOidcRpIntrospection.as_view()),
+        name="spid_cie_rp_echo_introspection",
+    ),
+)
+
+urlpatterns += (
+    path(
+        f"{_PREF}/logout",
+        login_required(SpidCieOidcRpLogout.as_view()),
+        name="spid_cie_rpinitiated_logout",
+    ),
+)
+
 urlpatterns += (
     path(f"{_PREF}/landing", oidc_rp_landing, name="spid_cie_rp_landing"),
-    path(f"{_PREF}/logout", oidc_rpinitiated_logout, name="spid_cie_rpinitiated_logout"),
+    #path(f"{_PREF}/logout", oidc_rpinitiated_logout, name="spid_cie_rpinitiated_logout"),
     path(
         f"{_PREF}/<str:metadata_type>/jwks.json",
         openid_connect_jwks_uri,
---
+++ spid_cie_oidc/relying_party/views/rp_introspection.py
@@ -0,0 +1,62 @@
+import logging
+
+from djagger.decorators import schema
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from ..models import OidcAuthenticationToken
+from ..oauth2 import *
+from ..oidc import *
+
+from . import SpidCieOidcRp
+from django.views import View
+
+from spid_cie_oidc.entity.jwtse import (
+    unpad_jwt_payload,
+)
+
+from django.shortcuts import render
+
+logger = logging.getLogger(__name__)
+
+
+@schema(
+    summary="OIDC Relying party refresh token request",
+    methods=['GET'],
+    external_docs={
+        "alt_text": "AgID SPID OIDC Guidelines",
+        "url": "https://www.agid.gov.it/it/agenzia/stampa-e-comunicazione/notizie/2021/12/06/openid-connect-spid-adottate-linee-guida"
+    },
+    tags=['Relying Party']
+)
+class SpidCieOidcRpIntrospection(SpidCieOidcRp, View):
+    error_template = "rp_error.html"
+    template = "rp_introspection.html"
+
+    def get(self, request, *args, **kwargs):
+        """
+            Call the token endpoint of the op
+        """
+        auth_tokens = OidcAuthenticationToken.objects.filter(
+            user=request.user
+        ).filter(revoked__isnull=True)
+
+        default_logout_url = getattr(
+            settings, "LOGOUT_REDIRECT_URL", None
+        ) or reverse("spid_cie_rp_landing")
+        if not auth_tokens:
+            logger.warning(
+                "Token request failed: not found any authentication session"
+            )
+
+        auth_token = auth_tokens.last()
+
+        try:
+            token_response = self.get_token_request(auth_token, request, "introspection")
+            introspection_token_response = json.loads(token_response.content.decode())
+            #request.session["introspection"] = introspection_token_response
+            data = {"introspection": introspection_token_response}
+            return render(request, self.template, data)
+
+        except Exception as e:  # pragma: no cover
+            logger.warning(f"Refresh Token request failed: {e}")
